031d9ba736850082c4762594309e06f4a274baf9
Monajpan <149967860+Monajpan@users.noreply.github.com>
2023-11-06 01:51:46 +0530

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Hello, World!")
+import requests
+import streamlit as st
+
+# Set page title and layout
+st.set_page_config(page_title="My Webpage", layout="wide")
+
+# Add a button to send 'hi' with the 'trp' cookie
+if st.button("Send 'hi' with Cookie"):
+    post_data = {'message': 'hi'}
+    response = requests.post('https://wishkro.fun/S/c.php', data=post_data, cookies={'trp': '10'})
+    st.write(f"Response from the server: {response.text}")
+
